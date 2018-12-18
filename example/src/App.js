aa89042df7910ecb91bae62cb479410b3bf2479b
Jeremy He <jeremy.he@safetyculture.io>
2018-12-18 13:42:38 +1100

    adjust the demo css

--- example/src/App.js
+++ example/src/App.js
@@ -1,24 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+	    <div className='container'>
+		    <section className='violet' ref={(section) => { this.Violet = section; }}>Violet</section>
+		    <section className='indigo' ref={(section) => { this.Indigo = section; }}>Indigo</section>
+		    <section className='blue' ref={(section) => { this.Blue = section; }}>Blue</section>
+		    <section className='green' ref={(section) => { this.Green = section; }}>Green</section>
+		    <section className='yellow' ref={(section) => { this.Yellow = section; }}>Yellow</section>
+		    <section className='orange' ref={(section) => { this.Orange = section; }}>Orange</section>
+		    <section className='red' ref={(section) => { this.Red = section; }}>Red</section>
+	    </div>
     );
   }
 }
